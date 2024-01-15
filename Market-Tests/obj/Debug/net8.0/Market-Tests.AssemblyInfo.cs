35edbe10aab9f084bbe5fe3fa1307348b28c5be7
admin <admin@Dimos-MBP.cust.communityfibre.co.uk>
2024-01-15 20:02:17 +0000

    feat: added initial test suits to fulfill the criteria

--- Market-Tests/obj/Debug/net8.0/Market-Tests.AssemblyInfo.cs
+++ Market-Tests/obj/Debug/net8.0/Market-Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Market-Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30eee0781065ab04966553ddef798cdd89cb71d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Market-Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Market-Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
